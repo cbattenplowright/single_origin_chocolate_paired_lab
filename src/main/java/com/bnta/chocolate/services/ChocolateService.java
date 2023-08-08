ea9331af8df1063f949081173e44bc95c79a5dfc
rohaib1 <rohaib192@gmail.com>
2023-08-08 16:50:23 +0100

    Created chocolate service

---
+++ src/main/java/com/bnta/chocolate/services/ChocolateService.java
@@ -0,0 +1,32 @@
+package com.bnta.chocolate.services;
+
+import com.bnta.chocolate.models.Chocolate;
+import com.bnta.chocolate.repositories.ChocolateRepository;
+import com.bnta.chocolate.repositories.EstateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ChocolateService {
+
+    @Autowired
+    ChocolateRepository chocolateRepository;
+    @Autowired
+    EstateRepository estateRepository;
+
+
+    public Chocolate saveChocolate(Chocolate chocolate){
+        return chocolateRepository.save(chocolate);
+    }
+
+    public List<Chocolate> getAllChocolate(){
+        return chocolateRepository.findAll();
+    }
+
+    public Optional<Chocolate> getChocolateById(Long id){
+        return chocolateRepository.findById(id);
+    }
+}
--- src/main/java/com/bnta/chocolate/services/ChocolatesService.java
+++
@@ -1,9 +0,0 @@
-package com.bnta.chocolate.services;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ChocolatesService {
-
-}
